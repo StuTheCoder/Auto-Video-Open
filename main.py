11a11e52bffd943b1d0815f042f9f8dc579c91c9
Stu <88785817+StuTheCoder@users.noreply.github.com>
2022-12-09 23:54:27 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import urllib, json
+from selenium import webdriver
+import time
+
+def look_for_new_video():
+    api_key = "AIzaSyDV7sbShh1IaBt9POaV_5AC9aWTnDFIhuA"
+    channel_id = "UCsUh-LIaJERYs7YghSNheuw"
+
+    base_video_url = 'https://www.youtube.com/watch?v='
+    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+    url = base_search_url + 'key={}&channelid={}&part=snippet,id&order=date&maxResults=1'.format(api_key, channel_id)
+    inp = urllib.urloppen(url)
+    resp = json.load(inp)
+
+    vidID = resp['items'][0]['id']['videoId']
+
+    video_exists = False
+    with open('videoid.json', 'r') as json_file:
+        data = json.load(json_file)
+        if data['videoId'] != vidID:
+            driver = webdriver.Chrome()
+            driver.get(base_video_url + vidID)
+            video_exists = True
+
+        if video_exists:
+            with open('videoid.json', 'w') as json_file:
+                data = {'videoId' : vidID}
+                json.dump(data, json_file)
+
+    try:
+        while True:
+            look_for_new_video()
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print('stopping')
